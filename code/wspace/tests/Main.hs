87c3a51277e46169d549742fc0da924a7fd48b5e
Israel Ahunanya <izyizy4good@gmail.com>
2025-10-20 16:46:46 +0000

    made the campain script to compile

--- code/wspace/tests/Main.hs
+++ code/wspace/tests/Main.hs
@@ -1,71 +1,71 @@
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE DeriveGeneric #-}
+-- {-# LANGUAGE NoImplicitPrelude #-}
+-- {-# LANGUAGE DataKinds #-}
+-- {-# LANGUAGE TemplateHaskell #-}
+-- {-# LANGUAGE ScopedTypeVariables #-}
+-- {-# LANGUAGE DeriveAnyClass #-}
+-- {-# LANGUAGE DeriveGeneric #-}
 
-module Main where
+-- module Main where
 
-import PlutusTx
-import PlutusTx.Prelude (traceIfFalse, BuiltinString, Bool(..), ($), Integer, (&&))
-import Plutus.V2.Ledger.Api 
-    ( Validator
-    , mkValidatorScript
-    , PubKeyHash
-    , POSIXTime
-    , ScriptContext
-    , TxInfo
-    , txInfoValidRange
-    )      
-import Plutus.V2.Ledger.Contexts ( txSignedBy)   
-import Prelude (Show, IO, putStrLn)
-import Plutus.V1.Ledger.Interval (contains, to, from)
-import PlutusTx.Builtins (toBuiltin)
+-- import PlutusTx
+-- import PlutusTx.Prelude (traceIfFalse, BuiltinString, Bool(..), ($), Integer, (&&))
+-- import Plutus.V2.Ledger.Api 
+--     ( Validator
+--     , mkValidatorScript
+--     , PubKeyHash
+--     , POSIXTime
+--     , ScriptContext
+--     , TxInfo
+--     , txInfoValidRange
+--     )      
+-- import Plutus.V2.Ledger.Contexts ( txSignedBy)   
+-- import Prelude (Show, IO, putStrLn)
+-- import Plutus.V1.Ledger.Interval (contains, to, from)
+-- import PlutusTx.Builtins (toBuiltin)
 
--- Every doner gets NFTs/benefits based on their level of donation
+-- -- Every doner gets NFTs/benefits based on their level of donation
 
---  Compaign Donation for specified beneficiaries
-data CampaignDatum = CampaignDatum {
-    beneficiary :: PubKeyHash,
-    goal        :: Integer,
-    deadline    :: POSIXTime
-} deriving (Show)
+-- --  Compaign Donation for specified beneficiaries
+-- data CampaignDatum = CampaignDatum {
+--     beneficiary :: PubKeyHash,
+--     goal        :: Integer,
+--     deadline    :: POSIXTime
+-- } deriving (Show)
 
-PlutusTx.unstableMakeIsData ''CampaignDatum -- converts data to bytes format (0 and 1s) that can be stored and transmitted -this because cardano doesnt understand haskell data structure it only understands binary data 
+-- PlutusTx.unstableMakeIsData ''CampaignDatum -- converts data to bytes format (0 and 1s) that can be stored and transmitted -this because cardano doesnt understand haskell data structure it only understands binary data 
 
-data CampaignActions = Donate | Withdraw | Refund 
-    deriving (Show)
+-- data CampaignActions = Donate | Withdraw | Refund 
+--     deriving (Show)
 
-PlutusTx.unstableMakeIsData ''CampaignActions -- converts data to bytes format (0 and 1s)
+-- PlutusTx.unstableMakeIsData ''CampaignActions -- converts data to bytes format (0 and 1s)
 
-mkCampaignValidator :: CampaignDatum -> CampaignActions -> ScriptContext -> Bool
-mkCampaignValidator dat red ctx = 
-    case red of
-        Donate   -> traceIfFalse (toBuiltin "Too late to donate") canDonate -- if canDonate is not true msg else run canDonate
-        Withdraw -> traceIfFalse (toBuiltin "Not allowed to withdraw") canWithdraw
-        Refund   -> traceIfFalse "Refund fail" refundCheck
+-- mkCampaignValidator :: CampaignDatum -> CampaignActions -> ScriptContext -> Bool
+-- mkCampaignValidator dat red ctx = 
+--     case red of
+--         Donate   -> traceIfFalse (toBuiltin "Too late to donate") canDonate -- if canDonate is not true msg else run canDonate
+--         Withdraw -> traceIfFalse (toBuiltin "Not allowed to withdraw") canWithdraw
+--         Refund   -> traceIfFalse "Refund fail" refundCheck
 
-        where 
-            info :: TxInfo -- TxInfo is a detailed record that contains all relevant information about the transaction
-            info = scriptContextTxInfo ctx --Retrieves scriptContextTxInfo from scriptt context (ctx) which returns TxInfo 
+--         where 
+--             info :: TxInfo -- TxInfo is a detailed record that contains all relevant information about the transaction
+--             info = scriptContextTxInfo ctx --Retrieves scriptContextTxInfo from scriptt context (ctx) which returns TxInfo 
 
-            canDonate :: Bool  -- - is the transaction time with the deadline
-            canDonate = to (deadline dat) `contains` (txInfoValidRange info) -- info provides  txInfoValidRange
+--             canDonate :: Bool  -- - is the transaction time with the deadline
+--             canDonate = to (deadline dat) `contains` (txInfoValidRange info) -- info provides  txInfoValidRange
 
-            canWithdraw :: Bool
-            canWithdraw = 
-                txSignedBy info (beneficiary dat) && --Check if the transaction was signed by the beneficiary specified in the datum.
-                from (deadline dat) `contains` txInfoValidRange info
+--             canWithdraw :: Bool
+--             canWithdraw = 
+--                 txSignedBy info (beneficiary dat) && --Check if the transaction was signed by the beneficiary specified in the datum.
+--                 from (deadline dat) `contains` txInfoValidRange info
 
-            refundCheck :: Bool
-            refundCheck = True 
+--             refundCheck :: Bool
+--             refundCheck = True 
 
-wrap :: CampaignDatum -> CampaignActions -> ScriptContext -> Bool
-wrap = mkCampaignValidator
+-- wrap :: CampaignDatum -> CampaignActions -> ScriptContext -> Bool
+-- wrap = mkCampaignValidator
 
-validator :: Validator
-validator = mkValidatorScript $$(PlutusTx.compile [|| wrap ||])
+-- validator :: Validator
+-- validator = mkValidatorScript $$(PlutusTx.compile [|| wrap ||])
 
 
 
@@ -108,58 +108,116 @@ validator = mkValidatorScript $$(PlutusTx.compile [|| wrap ||])
 -- main :: IO ()
 -- main = putStrLn "Campaign validator compiled successfully!"
 
--- {-# LANGUAGE NoImplicitPrelude #-}
--- {-# LANGUAGE DataKinds #-}
--- {-# LANGUAGE TemplateHaskell #-}
--- {-# LANGUAGE ScopedTypeVariables #-}
--- {-# LANGUAGE DeriveAnyClass #-}
--- {-# LANGUAGE DeriveGeneric #-}
-
--- module Main where
-
--- import Types
--- import PlutusTx (compile, unsafeFromBuiltinData, CompiledCode)
--- import PlutusTx.Builtins.Class (stringToBuiltinString)
--- import Plutus.V2.Ledger.Contexts (txSignedBy, ScriptContext)
--- import Plutus.V2.Ledger.Api (TxInfo, scriptContextTxInfo, Validator, mkValidatorScript,BuiltinData)
--- import Plutus.V1.Ledger.Interval (contains, to, from)
--- import PlutusTx.Prelude (traceIfFalse, Bool(..), (&&))
--- import Prelude (Show, IO, putStrLn)
 
 
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fplugin PlutusTx.Plugin #-}
 
--- {-# INLINABLE mkCampaignValidator #-}
--- mkCampaignValidator :: CampaignDatum -> CampaignActions -> ScriptContext -> Bool
--- mkCampaignValidator dat red ctx =
---     case red of
---         Donate   -> traceIfFalse (stringToBuiltinString "Too late to donate") canDonate
---         Withdraw -> traceIfFalse (stringToBuiltinString "Not allowed to withdraw") canWithdraw
---   where
---     info :: TxInfo
---     info = scriptContextTxInfo ctx
+module Main where
 
---     canDonate :: Bool
---     canDonate = to (deadline dat) `contains` txInfoValidRange info
+-- Plutus imports
+import PlutusTx (compile, unsafeFromBuiltinData, CompiledCode, unstableMakeIsData)
+import PlutusTx.Prelude hiding (($), (<>)) -- hide conflicting operators
+import Plutus.V2.Ledger.Contexts (txSignedBy, ScriptContext (..), TxInfo (..))
+import Plutus.V2.Ledger.Api (BuiltinData, Validator, mkValidatorScript, POSIXTime, PubKeyHash)
+import Plutus.V1.Ledger.Interval (contains, to, from)
 
---     canWithdraw :: Bool
---     canWithdraw =
---       txSignedBy info (beneficiary dat) &&
---       from (deadline dat) `contains` txInfoValidRange info
+-- Standard Haskell imports
+import Prelude (IO, Show, print, putStrLn, FilePath, ($), (<>))
+import GHC.Generics (Generic)
 
--- {-# INLINABLE wrap #-}
--- wrap :: CampaignDatum -> CampaignActions -> ScriptContext -> Bool
--- wrap = mkCampaignValidator
+-- File and serialization imports
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.ByteString.Short as SBS
+import Codec.Serialise (serialise)
+import Cardano.Api
+    ( writeFileTextEnvelope
+    , displayError
+    , PlutusScript (..)
+    , PlutusScriptV2
+    )
+import Cardano.Api.Shelley (PlutusScript (..))
 
--- compiledCode :: PlutusTx.CompiledCode (BuiltinData -> BuiltinData -> BuiltinData -> ())
--- compiledCode = $$(PlutusTx.compile [||
---     \d r c -> wrap
---         (PlutusTx.unsafeFromBuiltinData d)
---         (PlutusTx.unsafeFromBuiltinData r)
---         (PlutusTx.unsafeFromBuiltinData c)
---     ||])
+-- ===========================
+-- | Custom On-chain Types   |
+-- ===========================
 
--- validator :: Validator
--- validator = mkValidatorScript compiledCode
+data CampaignDatum = CampaignDatum
+    { beneficiary :: PubKeyHash
+    , deadline    :: POSIXTime
+    } deriving (Show, Generic)
 
--- main :: IO ()
--- main = putStrLn "Campaign validator compiled successfully!"
+data CampaignActions = Donate | Withdraw
+    deriving (Show, Generic)
+
+PlutusTx.unstableMakeIsData ''CampaignDatum
+PlutusTx.unstableMakeIsData ''CampaignActions
+
+-- ===========================
+-- | Validator Logic         |
+-- ===========================
+
+{-# INLINABLE mkCampaignValidator #-}
+mkCampaignValidator :: CampaignDatum -> CampaignActions -> ScriptContext -> Bool
+mkCampaignValidator dat red ctx =
+    case red of
+        Donate   -> traceIfFalse "Too late to donate" canDonate
+        Withdraw -> traceIfFalse "Not allowed to withdraw" canWithdraw
+  where
+    info :: TxInfo
+    info = scriptContextTxInfo ctx
+
+    canDonate :: Bool
+    canDonate = to (deadline dat) `contains` txInfoValidRange info
+
+    canWithdraw :: Bool
+    canWithdraw =
+        txSignedBy info (beneficiary dat) &&
+        from (deadline dat) `contains` txInfoValidRange info
+
+-- ===========================
+-- | Wrapper & Compilation   |
+-- ===========================
+
+{-# INLINABLE wrap #-}
+wrap :: BuiltinData -> BuiltinData -> BuiltinData -> ()
+wrap d r c =
+    let dat = unsafeFromBuiltinData d
+        red = unsafeFromBuiltinData r
+        ctx = unsafeFromBuiltinData c
+    in if mkCampaignValidator dat red ctx then () else error ()
+
+compiledCode :: PlutusTx.CompiledCode (BuiltinData -> BuiltinData -> BuiltinData -> ())
+compiledCode = $$(PlutusTx.compile [|| wrap ||])
+
+validator :: Validator
+validator = mkValidatorScript compiledCode
+
+-- ===========================
+-- | Write Validator to File |
+-- ===========================
+
+writeValidator :: FilePath -> Validator -> IO ()
+writeValidator file validator = do
+    let scriptSerialised = serialise validator
+        scriptShort = SBS.toShort . LBS.toStrict $ scriptSerialised
+        script = PlutusScriptSerialised scriptShort :: PlutusScript PlutusScriptV2
+    result <- writeFileTextEnvelope file Nothing script
+    case result of
+        Left err -> print (displayError err)
+        Right () -> putStrLn ("Wrote validator to: " <> file)
+
+-- ===========================
+-- | Main                    |
+-- ===========================
+
+main :: IO ()
+main = do
+    putStrLn "Validator compiled successfully!"
+    writeValidator "campaign-validator.plutus" validator
